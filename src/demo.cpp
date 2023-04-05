6729c9602282ee8878750d1747e4d8c785a456b5
David Dinh <dndinh7@gmail.com>
2023-04-05 11:55:37 -0400

    added a fly through camera

--- src/demo.cpp
+++ src/demo.cpp
@@ -19,6 +19,10 @@ struct Particle {
   float angleOffset; // angular offset
 };
 
+enum KEY {
+  W_KEY, A_KEY, S_KEY, D_KEY
+};
+
 class Viewer : public Window {
 public:
   Viewer() : Window() {
@@ -26,9 +30,17 @@ public:
 
   void setup() {
     setWindowSize(1000, 1000);
+    
+    renderer.loadShader("simple-texture",
+      "../shaders/simple-texture.vs",
+      "../shaders/simple-texture.fs");
+
     createSnowflake(1000);
     renderer.setDepthTest(false);
     renderer.blendMode(agl::ADD);
+    
+    renderer.loadTexture("snow", "../textures/snow.jpg", 0);
+    renderer.blendMode(agl::BLEND);
   }
 
   float randBound(float lowerBound, float upperBound) {
@@ -72,7 +84,7 @@ public:
       Particle particle = mParticles[i];
       particle.vel= vec3(cos(particle.freq * elapsedTime + particle.angleOffset), particle.vel.y, -sin(particle.freq * elapsedTime + particle.angleOffset));
       particle.pos += particle.vel * dt;
-      if (particle.pos.y <= 0) resetSnowflake(particle);
+      if (particle.pos.y - particle.size <= 0) resetSnowflake(particle);
       mParticles[i] = particle;
     }
   }
@@ -105,6 +117,14 @@ public:
   }
 
   void mouseMotion(int x, int y, int dx, int dy) {
+
+    // we're subtracting because it's opposite to the eyePos
+    azimuth-= (float)dx * 0.01f;
+    elevation-= (float)dy * 0.01f;
+
+    // clamp between (-pi/2, pi/2), don't want the bounds since it might lead to weird rotation
+    elevation= std::max(elevation, (float) (-M_PI/2) + 0.00001f);
+    elevation= std::min(elevation, (float) (M_PI/2)  - 0.00001f);
   }
 
   void mouseDown(int button, int mods) {
@@ -114,11 +134,67 @@ public:
   }
 
   void scroll(float dx, float dy) {
-    eyePos.x -= dy;
+
   }
 
   void keyUp(int key, int mods) {
+    if (key == GLFW_KEY_W) {
+      WASD_KEY_HELD[W_KEY]= false;
+    }
 
+    if (key == GLFW_KEY_A) {
+      WASD_KEY_HELD[A_KEY]= false;
+    }
+    
+    if (key == GLFW_KEY_S) {
+      WASD_KEY_HELD[S_KEY]= false;
+    }
+
+    if (key == GLFW_KEY_D) {
+      WASD_KEY_HELD[D_KEY]= false;
+    }
+  }
+
+  void keyDown(int key, int mods) {
+    if (key == GLFW_KEY_W) {
+      WASD_KEY_HELD[W_KEY]= true;
+    }
+
+    if (key == GLFW_KEY_A) {
+      WASD_KEY_HELD[A_KEY]= true;
+    }
+    
+    if (key == GLFW_KEY_S) {
+      WASD_KEY_HELD[S_KEY]= true;
+    }
+
+    if (key == GLFW_KEY_D) {
+      WASD_KEY_HELD[D_KEY]= true;
+    }
+  }
+
+  void updatePlayerPosition() {
+    if (WASD_KEY_HELD[W_KEY]) {
+      eyePos= eyePos - stepSize * eyeZAxis;
+    }
+
+    if (WASD_KEY_HELD[A_KEY]) {
+      eyePos= eyePos - stepSize * eyeXAxis;
+    }
+
+    if (WASD_KEY_HELD[S_KEY]) {
+      eyePos= eyePos + stepSize * eyeZAxis;
+    }
+
+    if (WASD_KEY_HELD[D_KEY]) {
+      eyePos= eyePos + stepSize * eyeXAxis;
+    }
+  }
+
+  void updateLookPos() {
+    lookPos.x= LOOK_RADIUS * sin(azimuth) * cos(elevation) + eyePos.x;
+    lookPos.y= LOOK_RADIUS * sin(elevation) + eyePos.y;
+    lookPos.z= LOOK_RADIUS * cos(azimuth) * cos(elevation) + eyePos.z;
   }
 
   void draw() {
@@ -126,26 +202,51 @@ public:
 
     float aspect = ((float)width()) / height();
     renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
-    
-    // draw plane
-    renderer.texture("Image", "stone");
-    renderer.push();
-    renderer.translate(vec3(0.0, -0.5, 0));
-    renderer.scale(vec3(2.0f));
-    renderer.plane();
-    renderer.pop();
+
+    updateLookPos();
 
     renderer.lookAt(eyePos, lookPos, up);
+
+    mat4 VM= renderer.viewMatrix();
+    eyeXAxis= vec3(VM[0][0], VM[1][0], VM[2][0]);
+    eyeYAxis= vec3(VM[0][1], VM[1][1], VM[2][1]);
+    eyeZAxis= vec3(VM[0][2], VM[1][2], VM[2][2]);
+
+    updatePlayerPosition();
+
     updateSnowflake(dt(), elapsedTime());
     drawSnowflake();
+    renderer.endShader();
+
+    renderer.beginShader("simple-texture");
+    // draw plane
+    renderer.texture("image", "snow");
+    renderer.push();
+    renderer.translate(vec3(0.0, -1, 0));
+    renderer.scale(vec3(30.0f, 1, 30.0f));
+    //renderer.plane();
+    renderer.pop();
     renderer.endShader();
   }
 
 protected:
 
-  vec3 eyePos = vec3(10, 0, 0);
-  vec3 lookPos = vec3(0, 0, 0);
+  vec3 eyePos = vec3(0, 2, 0);
+  vec3 lookPos = vec3(0, 2, 1);
   vec3 up = vec3(0, 1, 0);
+
+  float stepSize= 0.1;
+
+  vec3 eyeXAxis= vec3(1, 0, 0);
+  vec3 eyeYAxis= vec3(0, 1, 0);
+  vec3 eyeZAxis= vec3(0, 0, 1);
+
+  bool WASD_KEY_HELD[4]= {false, false, false, false};
+
+
+  float elevation= 0;
+  float azimuth= 0;
+  const float LOOK_RADIUS= 1;
 
   std::vector<Particle> mParticles;
 };
