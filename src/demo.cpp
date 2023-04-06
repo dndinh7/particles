a0e634a74b0aa7994cc381d3aa71d805d3dd4e8a
David Dinh <dndinh7@gmail.com>
2023-04-06 00:34:11 -0400

    got the flow field working :-)

--- src/demo.cpp
+++ src/demo.cpp
@@ -23,6 +23,158 @@ enum KEY {
   W_KEY, A_KEY, S_KEY, D_KEY
 };
 
+double clamp(double value, double low, double high) {
+  return std::max(std::min(value, high), low);
+}
+
+class Noise {
+  public:
+
+    Noise() {
+      initArrayP();
+    }
+
+    double noise(double x, double y, double z) {
+      int X= (int) floor(x) & 255;  // get the unit cube grid that contains (x,y,z)
+      int Y= (int) floor(y) & 255;
+      int Z= (int) floor(z) & 255;
+
+      x-= floor(x); // relative coordinates to the cube grid
+      y-= floor(y);
+      z-= floor(z);
+
+      // fade curves which smooths the transitions
+      double u= fade(x);
+      double v= fade(y);
+      double w= fade(z);
+
+      // hash coordinates of the corner of cubes
+      int A = p[X] + Y;
+      int AA= p[A] + Z;
+      int AB= p[A+1] + Z;
+      int B = p[X+1] + Y;
+      int BA= p[B] + Z;
+      int BB= p[B+1] + Z;
+
+      double gradAA= grad(p[AA], x, y, z);
+      double gradBA= grad(p[BA], x-1, y, z);
+      double gradAB= grad(p[AB], x, y-1, z);
+      double gradBB= grad(p[BB], x-1, y-1, z);
+      double gradAA_1= grad(p[AA+1], x, y, z-1);
+      double gradBA_1= grad(p[BA+1], x-1, y, z-1);
+      double gradAB_1= grad(p[AB+1], x, y-1, z-1);
+      double gradBB_1= grad(p[BB+1], x-1, y-1, z-1);
+
+      // first degree interpolation
+      double A_u= lerp(u, gradAA, gradBA);
+      double B_u= lerp(u, gradAB, gradBB);
+      double A1_u= lerp(u, gradAA_1, gradBA_1);
+      double B1_u= lerp(u, gradAB_1, gradBB_1);
+
+      // bilinear interpolation
+      double b= lerp(v, A_u, B_u);
+      double b_1= lerp(v, A1_u, B1_u);
+
+      // trilinear interpolation
+      double tri= lerp(w, b, b_1);
+
+      return tri;
+    }
+
+    static double fade(double t) {
+      return t * t * t * (t * ( t * 6 - 15) + 10);
+    }
+
+    static double lerp(double t, double a, double b) {
+      return a + t * (b-a);
+    }
+
+    static double grad(int hash, double x, double y, double z) {
+      int h= hash & 15;
+      double u= h < 8 ? x : y;
+      double v= (h < 4) ? y : (h == 12 || h == 14) ? x : z;
+
+      return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
+    }
+
+    static constexpr int permutation[] = {151,160,137,91,90,15,
+    131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+    190,6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+    88,237,149,56,87,174,20,125,136,171,168,68,175,74,165,71,134,139,48,27,166,
+    77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+    102,143,54,65,25,63,161,1,216,80,73,209,76,132,187,208,89,18,169,200,196,
+    135,130,116,188,159,86,164,100,109,198,173,186,3,64,52,217,226,250,124,123,
+    5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+    223,183,170,213,119,248,152,2,44,154,163,70,221,153,101,155,167,43,172,9,
+    129,22,39,253,19,98,108,110,79,113,224,232,178,185,112,104,218,246,97,228,
+    251,34,242,193,238,210,144,12,191,179,162,241,81,51,145,235,249,14,239,107,
+    49,192,214,31,181,199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,
+    138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180};
+
+    int p[512];
+
+  private:
+
+    void initArrayP() {
+      for (int i= 0; i < 256; i++) {
+        p[256+i]= p[i]= permutation[i];
+      }
+    }
+};
+
+constexpr int Noise::permutation[];
+
+class FlowField {
+  public:
+    FlowField() {
+      // should not be used
+    }
+
+    // inits the flowfield based on the noise factor
+    FlowField(Noise n, vec3 minPoint, vec3 maxPoint) {
+      this->minPoint= minPoint;
+      this->maxPoint= maxPoint;
+
+      dim= maxPoint - minPoint;
+      int size= dim.x * dim.y * dim.z;
+
+      double scale= 0.1;
+
+      velocityField= vector<vec3>(size, vec3(0));
+
+      for (int x= minPoint.x; x < maxPoint.x; x++) {
+        for (int y= minPoint.y; y < maxPoint.y; y++) {
+          for (int z= minPoint.z; z < maxPoint.z; z++) {
+            double noise= n.noise(x * scale, y * scale, z * scale); // 0 to 1
+            double angle= noise * 2 * M_PI; // then we map it to an angle
+            vec3 vel= vec3(cos(angle), sin(angle), noise);
+
+            int xIdx= x - minPoint.x;
+            int yIdx= y - minPoint.y;
+            int zIdx= z - minPoint.z;
+
+            velocityField[zIdx * dim.x * dim.y + yIdx * dim.x + xIdx]= vel;
+          }
+        }
+      }
+    }
+
+    vec3 getVel(vec3 p) {
+      int x= p.x - minPoint.x;
+      int y= p.y - minPoint.y;
+      int z= p.z - minPoint.z;
+
+      return velocityField[z * dim.x * dim.y + y * dim.x + x];
+    }
+
+    vec3 minPoint;
+    vec3 maxPoint;
+    vec3 dim; // dimensions of the cube/rectangular prism
+    vector<vec3> velocityField;
+};
+
+
+
 class Viewer : public Window {
 public:
   Viewer() : Window() {
@@ -35,7 +187,10 @@ public:
       "../shaders/simple-texture.vs",
       "../shaders/simple-texture.fs");
 
-    createSnowflake(1000);
+    noise= Noise();
+    flowField= FlowField(noise, vec3(-20, -20, -20), vec3(20, 20, 20));
+
+    createSnowflake(5000);
     renderer.setDepthTest(false);
     renderer.blendMode(agl::ADD);
     
@@ -56,25 +211,25 @@ public:
     for (int i = 0; i < size; i++)
     {
       Particle particle;
-      particle.color = vec4(1, 1, 1, 1);
-      particle.size = 0.15 + randBound(0, 0.15f);
+      particle.color = vec4(randomUnitCube(), 1);
+      particle.size = randBound(0.075, 0.15f);
       particle.rot = 0;
-      particle.pos = vec3(randBound(-10, 10), randBound(0, 20), randBound(-10, 10));
-      particle.vel = vec3(0, randBound(-0.8, -0.2), 0);
-      particle.freq= randBound(0.5, 1);
-      particle.amp= randBound(1, 2);
-      particle.angleOffset= randBound(-M_PI, M_PI);
+      particle.pos = vec3(randBound(-10, 10), randBound(-10, 10), randBound(-10, 10));
+      particle.vel = flowField.getVel(particle.pos);
+      //particle.freq= randBound(0.25, 1);
+      //particle.amp= randBound(1, 2);
+      //particle.angleOffset= randBound(-M_PI, M_PI);
       mParticles.push_back(particle);
 
     }
   }
 
   void resetSnowflake(Particle& particle) {
-    particle.pos = vec3(randBound(-10, 10), 20, randBound(-10, 10));
-    particle.vel = vec3(0, randBound(-0.8, -0.2), 0);
-    particle.freq= randBound(0.5, 1);
-    particle.amp= randBound(1, 2);
-    particle.angleOffset= randBound(-M_PI, M_PI);
+    particle.pos = vec3(randBound(-10, 10), randBound(-10, 10), randBound(-10, 10));
+    particle.vel = flowField.getVel(particle.pos);
+    //particle.freq= randBound(0.25, 1);
+    //particle.amp= randBound(1, 2);
+    //particle.angleOffset= randBound(-M_PI, M_PI);
   }
 
   void updateSnowflake(float dt, float elapsedTime)
@@ -82,9 +237,12 @@ public:
     for (int i = 0; i < mParticles.size(); i++)
     {
       Particle particle = mParticles[i];
-      particle.vel= vec3(cos(particle.freq * elapsedTime + particle.angleOffset), particle.vel.y, -sin(particle.freq * elapsedTime + particle.angleOffset));
+      particle.vel = flowField.getVel(particle.pos);
+      //particle.vel= vec3(cos(particle.freq * elapsedTime + particle.angleOffset), particle.vel.y, -sin(particle.freq * elapsedTime + particle.angleOffset));
       particle.pos += particle.vel * dt;
-      if (particle.pos.y - particle.size <= 0) resetSnowflake(particle);
+      if (particle.pos.x <= -15 || particle.pos.x >= 15) resetSnowflake(particle);
+      if (particle.pos.y <= -15 || particle.pos.y >= 15) resetSnowflake(particle);
+      if (particle.pos.z <= -15 || particle.pos.z >= 15) resetSnowflake(particle);
       mParticles[i] = particle;
     }
   }
@@ -242,6 +400,15 @@ protected:
   vec3 eyeZAxis= vec3(0, 0, 1);
 
   bool WASD_KEY_HELD[4]= {false, false, false, false};
+
+  FlowField flowField;
+  Noise noise;
+  vec3 flowFieldMinBound;
+  vec3 flowFieldMaxBound;
+
+  
+  vec3 particleMinBound;
+  vec3 particleMaxBound;
 
 
   float elevation= 0;
